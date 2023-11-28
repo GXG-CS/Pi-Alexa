5c1204215ded3febd9915014e63852408a183014
GXG <xiaoguang_guo@mines.edu>
2023-11-28 15:00:05 -0700

    remote_playAudio

--- data_collection/remote_playAudio.py
+++ data_collection/remote_playAudio.py
@@ -1,9 +1,8 @@
 import subprocess
 
-def run_remote_audio(remote_username, remote_ip, remote_audio_path):
+def run_remote_audio(remote_username, remote_ip, remote_audio_path, password):
     try:
-        # Command to activate conda environment and play audio
-        command = f"ssh {remote_username}@{remote_ip} 'source activate python3.9; afplay {remote_audio_path}'"
+        command = f"sshpass -p {password} ssh {remote_username}@{remote_ip} 'source activate python3.9; afplay {remote_audio_path}'"
         subprocess.run(command, shell=True, check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error running remote command: {e}")
@@ -12,8 +11,9 @@ def main():
     remote_username = "xiaoguang_guo@mines.edu"
     remote_ip = "192.168.1.232"
     remote_audio_path = "/Users/xiaoguang_guo@mines.edu/Documents/Pi-Alexa/data_collection/audio_A/1_audio.wav"
+    password = "338166"
 
-    run_remote_audio(remote_username, remote_ip, remote_audio_path)
+    run_remote_audio(remote_username, remote_ip, remote_audio_path, password)
 
 if __name__ == "__main__":
     main()
