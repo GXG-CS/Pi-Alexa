8946ba2700b88635ca820e9ef3989b46e32dafce
GXG <xiaoguang_guo@mines.edu>
2023-12-19 00:26:58 -0700

    traffic analysis

---
+++ data_collection/play_Capture.py
@@ -0,0 +1,41 @@
+import argparse
+import subprocess
+import threading
+import time
+
+def play_audio(file_path):
+    try:
+        # Use the aplay command to play the audio file
+        subprocess.run(['aplay', file_path])
+    except Exception as e:
+        print(f"Error playing audio: {e}")
+
+def capture_traffic(output_file, duration):
+    try:
+        # Use tcpdump to capture traffic for a specified duration, saving to the output file
+        subprocess.run(['sudo', 'tcpdump', '-w', output_file, '-G', str(duration), '-W', '1'])
+    except Exception as e:
+        print(f"Error capturing traffic: {e}")
+
+if __name__ == "__main__":
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='Play a WAV file and capture traffic.')
+    parser.add_argument('file_path', type=str, help='Path to the WAV file to play')
+    parser.add_argument('output_file', type=str, help='Path to save the traffic capture file')
+    parser.add_argument('--duration', type=int, default=30, help='Duration to capture traffic (in seconds)')
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Start traffic capture in a separate thread
+    capture_thread = threading.Thread(target=capture_traffic, args=(args.output_file, args.duration))
+    capture_thread.start()
+
+    # Delay to ensure traffic capture starts before playing audio
+    time.sleep(2)
+
+    # Play the audio
+    play_audio(args.file_path)
+
+    # Wait for the capture thread to finish
+    capture_thread.join()
---
+++ data_collection/traffic_analysis.py
@@ -0,0 +1,66 @@
+from scapy.all import rdpcap, Ether
+import matplotlib.pyplot as plt
+import argparse
+import datetime
+
+def analyze_traffic(pcap_file, device_mac):
+    packets = rdpcap(pcap_file)
+    timestamps_incoming = []
+    timestamps_outgoing = []
+    sizes_incoming = []
+    sizes_outgoing = []
+
+    for packet in packets:
+        if Ether in packet:
+            packet_time = datetime.datetime.fromtimestamp(packet.time)
+            packet_size = len(packet) * 8 / 1024  # Convert bytes to Kilobits
+
+            # Check if the packet is incoming or outgoing based on MAC address
+            if packet[Ether].dst.lower() == device_mac.lower():
+                timestamps_incoming.append(packet_time.timestamp())
+                sizes_incoming.append(packet_size)
+            elif packet[Ether].src.lower() == device_mac.lower():
+                timestamps_outgoing.append(packet_time.timestamp())
+                sizes_outgoing.append(packet_size)
+
+    return timestamps_incoming, sizes_incoming, timestamps_outgoing, sizes_outgoing
+
+def aggregate_traffic(timestamps, sizes):
+    start_time = min(timestamps)
+    end_time = max(timestamps)
+    interval = 1  # Aggregate data in 1-second intervals
+    rates = []
+
+    for i in range(int(start_time), int(end_time) + 1, interval):
+        bytes_in_interval = sum(size for time, size in zip(timestamps, sizes) if i <= time < i + interval)
+        rates.append(bytes_in_interval)  # Traffic rate in Kilobits per second
+
+    return rates
+
+def plot_traffic_curve(incoming_rates, outgoing_rates):
+    plt.figure(figsize=(12, 6))
+    plt.plot(incoming_rates, label='Incoming', marker='x')
+    plt.plot(outgoing_rates, label='Outgoing', marker='o')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Traffic Rate (Kb/s)')
+    plt.title('Traffic Rate over Time')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Analyze a pcap file and plot traffic rates for a given MAC address.')
+    parser.add_argument('pcap_file', type=str, help='The path to the pcap file to analyze')
+    parser.add_argument('device_mac', type=str, help='The MAC address of the Alexa device')
+
+    args = parser.parse_args()
+
+    timestamps_incoming, sizes_incoming, timestamps_outgoing, sizes_outgoing = analyze_traffic(args.pcap_file, args.device_mac)
+
+    if timestamps_incoming and timestamps_outgoing:
+        incoming_rates = aggregate_traffic(timestamps_incoming, sizes_incoming)
+        outgoing_rates = aggregate_traffic(timestamps_outgoing, sizes_outgoing)
+        plot_traffic_curve(incoming_rates, outgoing_rates)
+    else:
+        print("No traffic found for the specified MAC address.")
